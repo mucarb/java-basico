f79733f7379cbf6594f065135322ce8307b19734
mucarb <murilo.rb60@gmail.com>
2024-05-13 19:57:47 -0300

    imprimindo mensagens

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,7 +6,13 @@ public class ContaTerminal {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    // TODO: Imprimir as mensagens para o usuário
+    System.out.println("Informer seu nome!");
+
+    System.out.println("Informe o número de sua conta!");
+
+    System.out.println("Qual o número de sua agencia? Ex.: 000-0");
+
+    System.out.println("Quantos você tem de saldo?");
 
     // TODO: Receber os dados pelo scanner
 
