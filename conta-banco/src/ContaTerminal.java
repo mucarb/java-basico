3c61e62e268c4c93e6f17a740a70bb33d48a51ef
mucarb <murilo.rb60@gmail.com>
2024-05-13 19:56:48 -0300

    instanciando a classe Scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,13 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
   public static void main(String[] args) {
-    // TODO: Importar a classe Scanner para entrada de dados
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     // TODO: Imprimir as mensagens para o usuário
 
     // TODO: Receber os dados pelo scanner
 
     // TODO: Imprimir resultado dos dados da conta ao usuário
+
+    scanner.close();
   }
 
 }
